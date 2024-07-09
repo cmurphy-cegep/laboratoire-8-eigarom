0a87bc4dbb10453f7fbeca50c975b40e2f855bb1
Romain Billot <206266704@cegepsherbrooke.qc.ca>
2024-07-09 19:43:40 -0400

    exercice2.1

--- lab8_frontend/src/pages/FormulaireLogin.vue
+++ lab8_frontend/src/pages/FormulaireLogin.vue
@@ -9,27 +9,56 @@
     Référez-vous au composant FormulaireLogin.vue de l'exemple du cours 19 pour plus de détails.
 -->
 <template>
-    <div class="boxed-left">
-        À compléter ...
-    </div>
+	<div class="boxed-left">
+		<form @submit.prevent="login">
+			<div>
+				<label for="username">Compte utilisateur: </label>
+				<input id="username" v-model="username" name="username" type="text"/>
+			</div>
+			<div>
+				<label for="password">Mot de passe: </label>
+				<input id="password" type="password" v-model="password" name="password"/>
+			</div>
+			<button>Se connecter</button>
+		</form>
+	</div>
 </template>
 
 <script>
-  // À compléter ...
+import session from '../session';
+
+export default {
+	data: function () {
+		return {
+			username: '',
+			password: ''
+		};
+	},
+	methods: {
+		login() {
+			session.login(this.username, this.password).then(user => {
+				alert("Bienvenue, " + user.username + (user.isAdmin ? ".\nVous êtes administrateur." : "."));
+				this.$router.push('/');
+			}).catch(authError => {
+				alert(authError.message);
+			});
+		}
+	}
+}
 </script>
 
 <style scoped>
 form * {
-    margin: 0.3rem;
+	margin: 0.3rem;
 }
 
 .boxed-left {
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-    margin: 1rem auto;
-    border-radius: 10px;
-    padding: 1rem;
-    text-align: left;
-    width: 90%;
-    max-width: 80rem;
+	box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+	margin: 1rem auto;
+	border-radius: 10px;
+	padding: 1rem;
+	text-align: left;
+	width: 90%;
+	max-width: 80rem;
 }
 </style>
